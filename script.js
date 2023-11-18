bb5b8633421f6b26fae8e0189d2c0bd0ffe221a5
johnysantos22 <santosjohny302@gmail.com>
2023-11-18 19:33:38 -0300

    Responsividade-do-menu-mobile

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('btn-menu')
+const nav = document.getElementById('navegacao-mobile')
+function animarMenu(){
+    nav.classList.toggle('abrir')
+    btn.classList.toggle('ativar')
+}
